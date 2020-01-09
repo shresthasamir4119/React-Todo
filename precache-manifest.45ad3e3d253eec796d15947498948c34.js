a6e97222ed0fc36bf357b4cb0ed0e41511374caa
shresthasamir4119 <shresthausui@icloud.com>
2020-01-09 16:13:07 +0545

    Updates

---
+++ precache-manifest.45ad3e3d253eec796d15947498948c34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fda0efa79713f837797e11ff74f163a",
+    "url": "/React-Todo/index.html"
+  },
+  {
+    "revision": "d983c4bc698a94049f56",
+    "url": "/React-Todo/static/css/main.5a006230.chunk.css"
+  },
+  {
+    "revision": "b6e91ae093e6dc46e22c",
+    "url": "/React-Todo/static/js/2.57fe3756.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Todo/static/js/2.57fe3756.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d983c4bc698a94049f56",
+    "url": "/React-Todo/static/js/main.67e639f9.chunk.js"
+  },
+  {
+    "revision": "b78b07ade98e8ff93f08",
+    "url": "/React-Todo/static/js/runtime-main.e6f5c554.js"
+  }
+]);
